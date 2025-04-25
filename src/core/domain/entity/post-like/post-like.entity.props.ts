c730ef27c1d08678587657fb29d0e2157a4438af
Gabriel Flores <gabriel.flores@epiousion.com.br>
2025-04-25 17:34:48 -0300

    :zap: add: post-like entity

--- src/core/domain/entity/post-like/post-like.entity.props.ts
+++ src/core/domain/entity/post-like/post-like.entity.props.ts
@@ -1,8 +1,9 @@
 import type { AbstractEntityPropsInterface } from '@base/core/domain/entity/abstract/abstract.entity.props';
+import type { PostEntity } from '@base/core/domain/entity/post/post.entity';
 import type { UserEntity } from '@base/core/domain/entity/user/user.entity';
 
-export interface PostEntityPropsInterface extends AbstractEntityPropsInterface {
-  image: string;
-  description: string;
+export interface PostLikeEntityPropsInterface
+  extends AbstractEntityPropsInterface {
+  post: PostEntity;
   user: UserEntity;
 }
--- src/core/domain/entity/post-like/post-like.entity.ts
+++ src/core/domain/entity/post-like/post-like.entity.ts
@@ -1,26 +1,27 @@
 import { AbstractEntity } from '@base/core/domain/entity/abstract/abstract.entity';
 import { Ok } from '@base/shared/feature/functional/ok.function';
 
-import type { PostEntityPropsInterface } from '@base/core/domain/entity/post/post.entity.props';
+import type { PostEntity } from '@base/core/domain/entity/post/post.entity';
+import type { PostLikeEntityPropsInterface } from '@base/core/domain/entity/post-like/post-like.entity.props';
 import type { UserEntity } from '@base/core/domain/entity/user/user.entity';
 import type { Either } from '@base/shared/feature/functional/either.type';
 
-export class PostEntity extends AbstractEntity {
-  public image: string;
-  public description: string;
+export class PostLikeEntity extends AbstractEntity {
+  public post: PostEntity;
   public user: UserEntity;
 
-  protected readonly _type = PostEntity.name;
+  protected readonly _type = PostLikeEntity.name;
 
-  private constructor(props: PostEntityPropsInterface) {
+  private constructor(props: PostLikeEntityPropsInterface) {
     super(props);
 
-    this.image = props.image;
-    this.description = props.description;
+    this.post = props.post;
     this.user = props.user;
   }
 
-  static create(props: PostEntityPropsInterface): Either<never, PostEntity> {
-    return Ok(new PostEntity(props));
+  static create(
+    props: PostLikeEntityPropsInterface,
+  ): Either<never, PostLikeEntity> {
+    return Ok(new PostLikeEntity(props));
   }
 }
