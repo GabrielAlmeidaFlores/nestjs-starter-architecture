4065c1634d80d23493eaa51517c304b600f72441
Gabriel Flores <gabriel.flores@epiousion.com.br>
2025-04-30 04:27:29 -0300

    :hammer: refactor: base repositoy interface

--- src/core/domain/repository/base.repository.interface.ts
+++ src/core/domain/repository/base.repository.interface.ts
@@ -1,46 +1,34 @@
 import type { BaseEntity } from '@base/core/domain/entity/base/base.entity';
 import type { DeepPartial } from '@base/core/domain/repository/type/deep-partial.type';
+import type { Guid } from '@core/domain/entity/base/value-object/guid.value-object';
 import type { EntityNotFoundError } from '@core/domain/repository/error/entity-not-found.error';
-import type { ForeignKeyViolationError } from '@core/domain/repository/error/foreign-key-violation.type';
 import type { QueryFailedError } from '@core/domain/repository/error/query-failed.error';
-import type { UniqueConstraintError } from '@core/domain/repository/error/unique-constraint.error';
 import type { FindOptionsInterface } from '@core/domain/repository/interface/find-options.interface';
 import type { ListOptionsInterface } from '@core/domain/repository/interface/list-options.interface';
 import type { ListedResultInterface } from '@core/domain/repository/interface/listed-result.interface';
 import type { Either } from '@shared/feature/functional/either.type';
 
 export interface BaseRepositoryInterface<T extends BaseEntity> {
-  create(
-    data: T,
-  ): Either<
-    ForeignKeyViolationError | UniqueConstraintError | QueryFailedError,
-    Promise<void>
-  >;
+  create(data: T): Promise<Either<QueryFailedError, void>>;
 
   update(
-    where: DeepPartial<T>,
+    id: Guid,
     data: DeepPartial<T>,
-  ): Either<
-    | EntityNotFoundError
-    | ForeignKeyViolationError
-    | UniqueConstraintError
-    | QueryFailedError,
-    Promise<void>
-  >;
+  ): Promise<Either<EntityNotFoundError | QueryFailedError, void>>;
 
   delete(
-    where: DeepPartial<T>,
-  ): Either<EntityNotFoundError | QueryFailedError, Promise<void>>;
+    id: Guid,
+  ): Promise<Either<EntityNotFoundError | QueryFailedError, void>>;
 
   findOne(
     options: FindOptionsInterface<T>,
-  ): Either<EntityNotFoundError | QueryFailedError, Promise<T>>;
+  ): Promise<Either<EntityNotFoundError | QueryFailedError, T>>;
 
   find(
     options: FindOptionsInterface<T>,
-  ): Either<QueryFailedError, Promise<T[]>>;
+  ): Promise<Either<QueryFailedError, T[]>>;
 
   list(
     options: ListOptionsInterface,
-  ): Either<QueryFailedError, Promise<ListedResultInterface<T>>>;
+  ): Promise<Either<QueryFailedError, ListedResultInterface<T>>>;
 }
--- src/core/domain/repository/error/unique-constraint.error.ts
+++
@@ -1,5 +0,0 @@
-import { InvalidInputError } from '@shared/feature/functional/error/invalid-input.error';
-
-export class UniqueConstraintError extends InvalidInputError {
-  protected readonly _type = UniqueConstraintError.name;
-}
