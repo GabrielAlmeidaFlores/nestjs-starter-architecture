b60031dea40da6496b991ffc06ba381245ac7c96
Gabriel Flores <gabriel.flores@epiousion.com.br>
2025-04-30 06:26:08 -0300

    :sparkles: add: functional guards

---
+++ src/shared/feature/functional/guard/is-fail.guard.ts
@@ -0,0 +1,9 @@
+import type { BaseError } from '@shared/feature/functional/error/base.error';
+import type { Failure } from '@shared/feature/functional/object/failure.object';
+import type { Either } from '@shared/feature/functional/type/either.type';
+
+export function isFailure<E extends BaseError, T>(
+  either: Either<E, T>,
+): either is Failure<E> {
+  return either.isFailure();
+}
