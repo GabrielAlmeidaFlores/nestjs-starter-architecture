672656550b2f7a8f05de54d9435659f02100b8ea
Gabriel Flores <gabriel.flores@epiousion.com.br>
2025-05-02 07:39:59 -0300

    :zap: update: deep-partial-object

--- src/core/domain/repository/type/partial/deep-partial-object.type.ts
+++ src/core/domain/repository/type/partial/deep-partial-object.type.ts
@@ -1,3 +1,5 @@
+import type { ValueObjectInterface } from '@core/domain/value-object/value-object.interface';
+
 type PrimitiveType =
   | string
   | number
@@ -10,8 +12,14 @@ type PrimitiveType =
 
 export type DeepPartialObjectType<T> = T extends PrimitiveType
   ? T
-  : T extends object
-    ? Partial<{
-        [K in keyof T]: DeepPartialObjectType<T[K]>;
-      }>
-    : T;
+  : T extends ValueObjectInterface<unknown>
+    ? T
+    : T extends object
+      ? {
+          [K in keyof T]: T[K] extends PrimitiveType
+            ? T[K]
+            : T[K] extends ValueObjectInterface<unknown>
+              ? T[K]
+              : T[K] | undefined;
+        }
+      : T;
